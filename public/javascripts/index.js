0171dd463d08044939204e95232f10c050666d07
KU Lok Sun <sundaykuloksun@gmail.com>
2017-11-20 16:15:09 +0800

    Add cross origin test

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -31,3 +31,19 @@ $('#same-origin-invalid-origin').click(function() {
   get("/access-control-allow-origin/invalid", false);
 });
 
+$('#hardcoded-same-origin-no-origin').click(function() {
+  get("//local.example.com/access-control-allow-origin/no", false);
+});
+$('#hardcoded-same-origin-all-origin').click(function() {
+  get("//local.example.com/access-control-allow-origin/all", false);
+});
+$('#hardcoded-same-origin-correct-origin').click(function() {
+  get("//local.example.com/access-control-allow-origin/local", false);
+});
+$('#hardcoded-same-origin-wrong-origin').click(function() {
+  get("//local.example.com/access-control-allow-origin/remote", false);
+});
+$('#hardcoded-same-origin-invalid-origin').click(function() {
+  get("//local.example.com/access-control-allow-origin/invalid", false);
+});
+
